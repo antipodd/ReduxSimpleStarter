54d845aa9e8c6fc4955b92cdedb9b7669948817c
Karel <karel.hartlieb@gmail.com>
2017-08-13 12:43:25 -0500

    2-29

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
 	const imageUrl = video.snippet.thumbnails.default.url;
 
 	//console.log(video)
@@ -9,7 +9,7 @@ const VideoListItem = ({video}) => {
 	//the first object in the arguments array has a property video
 	//grab the video and declare a new variable called video
 	return (
-		<li className="list-group-item">
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
 			<div className="video-list media">
 				<div className="media-left">
 					<img className="media-object" src={imageUrl}/>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = "AIzaSyCMgpQPH3y3iW657cxkSY3VqmiW8jAUKB4";
 
@@ -13,11 +14,17 @@ import SearchBar from './components/search_bar';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
 			//console.log(data)
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 			 //ES6 - whenever key and value are the same variable name can use name of key in curly braces i.e. { videos } 
 			 //	this.setState({ videos: videos }) - syntactic sugar	
 					});
@@ -27,7 +34,10 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+				videos={this.state.videos} />
 			</div>
 		);
 	}
